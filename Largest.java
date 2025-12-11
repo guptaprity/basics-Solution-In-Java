a35b56cbd4263e7ac0e1deecf10a981f49bfcf76
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-12-11 22:06:56 +0530

    Create  Largest.java

---
+++ Largest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class  Largest
+{
+	
+	public static int getLargest(int num[]) {
+		int l = Integer.MIN_VALUE;
+		for(int i =0; i<num.length; i++) {
+			if(l<num[i]) {
+				l=num[i];
+			}
+		}
+		return l;
+	}
+	public static void main(String[] args) 
+	{
+		int num[] = {5, 6, 7, 8, 9};
+		System.out.println("Largest Value"+getLargest(num));
+	}
+}
